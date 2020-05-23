3b63a88081564bc26b929a58e50fd59ef0c8cf61
Chase00 <chasekuhnmc@gmail.com>
2020-05-23 15:36:55 -0400

    Navbar component & removed starter react files

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { MdColorLens, MdAccountCircle } from 'react-icons/md';
+import './Navbar.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar expand="sm" color="dark" class="navbar bg-dark">
+        <Router>
+        <NavbarBrand href="/"><MdColorLens size={32}/> Color Vault</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink><Link to="/">Colors</Link></NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink><Link to="/">About</Link></NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText><MdAccountCircle size={40}/> Sign Out</NavbarText>
+        </Collapse>
+        <Switch>
+              <Route exact path="/home"></Route>
+              <Route exact path="/colors"></Route>
+              <Route exact path="/about"></Route>
+      </Switch>
+        </Router>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
