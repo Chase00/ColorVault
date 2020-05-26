3cd3b2d11c9fb8d8c13992f879d094dce631ef78
Chase00 <chasekuhnmc@gmail.com>
2020-05-25 21:05:27 -0400

    Footer Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Navbar />
+
+
+      
+      <Footer />
     </div>
   );
 }
