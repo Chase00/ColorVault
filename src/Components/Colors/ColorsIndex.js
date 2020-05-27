6268948f9695134600a387f27707e2ed3f692517
Chase00 <chasekuhnmc@gmail.com>
2020-05-26 21:23:04 -0400

    Color requests
    Returning all colors that belong to a user

--- src/Components/Colors/ColorsIndex.js
+++ src/Components/Colors/ColorsIndex.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ColorsIndex = (props) => {
+    const [colors, setColors] = useState([]);
+
+    const fetchColors = () => {
+        fetch('http://localhost:3000/api/color', {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
+        }) .then((res) => res.json())
+        .then((colorData) => {
+            console.log(colorData)
+            setColors(colorData)
+        })
+    }
+
+    useEffect(() => {
+        fetchColors();
+    }, [])
+
     return(
         <div>
-            Colors Index!
+            User's colors will go here
         </div>
     )
 }
