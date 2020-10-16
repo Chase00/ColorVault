a8963559b5e7db732580dca5fdd90d616541a234
Chase00 <chasekuhnmc@gmail.com>
2020-10-16 14:25:18 -0400

    Version 1.1.0

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer class="footer">
         <hr class="divider"></hr>
         <p class="footer-text"> &copy; 2020 Color Vault - All Rights Reserved.</p>
-        <p class="footer-text"> Version 1.0.0</p>
+        <p class="footer-text"> Version 1.1.0</p>
         <a href="https://github.com/Chase00/ColorVaultClient" target="_blank"><img class="footer-icons" src="/images/github.png" alt="github link"></img></a>
         <a href="https://chasekuhn.com" target="_blank"><img class="footer-icons" src="/images/link.png" alt="portfolio link"></img></a>
       </footer>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
                 <Col md="6">
                     <h1 class="home-title">Color Vault</h1>
                     <h2 class="home-second">Store your colors with ease.</h2>
-                    <p class="home-text">Providing a free and easy solution for keeping track of your digital color codes. Whether you're a graphic designer working on the next eye-catching ad or web designer creating a new site layout, this product is for you!</p>
+                    <p class="home-text">Providing a free and easy solution for keeping track of your digital color codes. Whether you're a graphic designer working on the next eye-catching ad or a web designer creating a new site layout, Color Vault is for you!</p>
                     <Link to="/signup"><button class="button-main">Sign Up</button></Link>
                     <Link to="/login"><button class="button-secondary">Log in</button></Link>
                 </Col>
